8afed99d2eb3575a5ba1939fcf8325ccc96695ce
mbuguapurity <mbuguapurity1@gmail.com>
2019-03-14 01:18:31 +0300

    add file

---
+++ lists quiz 2.py
@@ -0,0 +1,162 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> numbers=list(range(1,11)
+		 print(sum(numbers))
+		 
+
+>>> list=[1,2,3,4]
+		 
+>>> print(sum(list))
+		 
+10
+>>> a=[1,1,-2,45]
+		 
+>>> sum=sum(a)
+		 
+>>> a
+		 
+[1, 1, -2, 45]
+>>> print(sum(a))
+		 
+
+>>> print(sum(a))
+		 
+
+>>> sum
+		 
+45
+>>> n=[1,2,3,4,5]
+		 
+>>> b=sum(n)
+		 
+
+>>> a=[2,5,4]
+		 
+>>> print(max(a))
+		 
+5
+>>> print(min(a))
+		 
+2
+>>> def getKey(items):
+		 return items[1]
+		 a=[(2,3),(6,7),(3,34),(24,64),(1,43)]
+		 l=sorted(a,Key=getKey)
+
+		 
+>>> print(l)
+		 
+
+>>> 1
+		 
+1
+>>> items
+		 
+
+>>> l=[(2,5),(1,2),(4,4),(2,3),(2,1)]
+		 
+>>> defgetKey(item):
+		 
+
+>>> def getkey(item):
+		 return item[1]
+		 sorted(l,key =getkey)
+
+		 
+>>> l
+		 
+[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+>>> 1
+		 
+1
+>>> list=[2,4,1,4,2,8,8]
+		 
+>>> list=set(list)
+		 
+>>> print(list)
+		 
+{8, 1, 2, 4}
+>>> a=[10,20,30,20,10,50,60,40,80,50,40]
+		 
+>>> list(set(a))
+		 
+
+>>> b=[5,6,4,7,2,9,10,5,6,7,101,11,51,9]
+		 
+>>> b=set(b)
+		 
+>>> b
+		 
+{2, 4, 5, 6, 7, 101, 9, 10, 11, 51}
+>>> num=[[11,2,3],[4,5,6],[10,11,12],[7,8,9]]
+		 
+>>> print(max(num))#==>[11,2,3]
+		 
+[11, 2, 3]
+>>> a=b=[10,20,30]
+		 
+>>> a=[10,20,30]
+		 
+>>> b=[40,50,60]
+		 
+>>> print(a+b)
+		 
+[10, 20, 30, 40, 50, 60]
+>>> num=[[10,20],[40],[30,56,25],[10,20],[33],[40]]
+		 
+>>> n2=[]
+		 
+>>> for i in num:
+		 if in not in n2:
+		 
+SyntaxError: invalid syntax
+>>> if i not in n2:
+		 n2.append(i)
+
+		 
+
+>>> num=[[11,2,3],[4,5,6],[10,11,12],[7,8,9]]
+		 
+>>> color="["red","green","white"]"
+		 
+
+>>> color="["Red","Green","White"]"
+		 
+
+>>> s="python"
+		 
+>>> l=[]
+		 
+>>> l.append(s)
+		 
+>>> l
+		 
+['python']
+>>> a={0:10,1:20}
+		 
+>>> a[2]=30
+		 
+>>> a
+		 
+{0: 10, 1: 20, 2: 30}
+>>> a={1:10,2:20}
+		 
+>>> b={3:30,4:40}
+		 
+>>> c={5:50,6:60}
+		 
+>>> d={}
+		 
+>>> for d in(a,b,c):
+		 d.update(d)
+
+		 
+>>> d
+		 
+{5: 50, 6: 60}
+>>> a=(1,2,3,4,5,6,7,8,9,10,11,12)
+		 
+>>> b=list(a)
+		 
+
+>>> 
